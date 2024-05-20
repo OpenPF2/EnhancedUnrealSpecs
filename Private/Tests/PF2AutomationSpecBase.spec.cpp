c1ac81e3e16aa56e665d2636856f0b7a545c163e
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2024-05-20 16:32:59 -0400

    Re-license OpenPF2 Automation Spec Code from MPL to MIT License
    This expands the reach of the test enhancements implemented by the
    OpenPF2 project, allowing them to be used freely in proprietary,
    closed-source projects.

--- Private/Tests/PF2AutomationSpecBase.spec.cpp
+++ Private/Tests/PF2AutomationSpecBase.spec.cpp
@@ -1,7 +1,7 @@
-﻿// OpenPF2 for UE Game Logic, Copyright 2023-2024, Guy Elsmore-Paddock. All Rights Reserved.
+﻿// Better Automation Specs for UE, Copyright 2023-2024, Guy Elsmore-Paddock. All Rights Reserved.
 //
-// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
-// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// This Source Code Form is subject to the terms of the MIT License. If a copy of the license was not distributed with
+// this file, you can obtain one at https://github.com/OpenPF2/BetterUnrealSpecs/blob/main/LICENSE.txt.
 
 #include <GameplayEffect.h>
 
